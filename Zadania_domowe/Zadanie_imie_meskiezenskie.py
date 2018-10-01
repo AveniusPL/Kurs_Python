0b103e605f1d44342fc8203812c49f03a16e6e63
Patrik <patryk.janusz@interia.pl>
2018-10-01 19:01:07 +0200

    Dodanie wykluczenia imienia.

--- Zadania_domowe/Zadanie_imie_meskiezenskie.py
+++ Zadania_domowe/Zadanie_imie_meskiezenskie.py
@@ -3,7 +3,7 @@
 imie = input("Podaj swoje imię : ")
 def sprawdzanie():
     global imie
-    if imie.endswith("a"):
+    if imie.endswith("a") and imie!= "Jan Maria":
         print ("To jest imię damskie.")
     else:
         print("To jest imie męskie.")
