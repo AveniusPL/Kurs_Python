68f2b36b3b5ddd71a7f2e6423560e26d8e916964
Patrik <patryk.janusz@interia.pl>
2018-10-01 13:42:02 +0200

    Gotowy program do operacji na łancuchach znaków.

--- Zadania_domowe/Zamianaznakow.py
+++ Zadania_domowe/Zamianaznakow.py
@@ -1,30 +1,41 @@
-zdanie = input("Podaj swoje zdanie : ")
+print("\nWitaj w programie do operacji na łancuchach znaków\n")
+def wstep():
+    global zdanie
+    zdanie = input("Podaj swoje zdanie : ")
 
 def menu():
+    print ( "MENU : ")
     print (" 1 - Zamienić litery na duże.")
     print (" 2 - Zammień litery na małe")
-    print (" 3 - Zamień wybrane znaki w zdaniu na inne")
+    print (" 3 - Zamień wybrane znaki w zdaniu na inne\n")
 
 def zamiana_duze():
-    zdanie.upper()
-    print ("O to Twoje zdanie : " + zdanie)
+    global zdanie
+    zdanie = zdanie.upper()
+    print ("Twoje zdanie : " + zdanie)
     return
 
 def zamiana_male():
-    znak_dozamiany = input("Wpisz znak jaki chcesz zamienić")
-    podmieniajacy = input("Wpisz znak na który chcesz zamienić")
+    global zdanie
+    zdanie = zdanie.lower()
+    print ("Twoje zdanie : " + zdanie)
     return
 
-def zamiana_inne():
-    znak_dozamiany = input("Wpisz znak jaki chcesz zamienić")
-    podmieniajacy = input("Wpisz znak na który chcesz zamienić")
+def zamiana_znaku():
+    global zdanie
+    znak = input("podaj znak do zamiany : ")
+    zamiana = input("podaj znak zamieniajacy : ")
+    zdanie = zdanie.replace (znak, zamiana )
+    print (zdanie)
     return
-
-
+wstep()
 menu()
-operacja = input("Co wybierasz? ")
+operacja = input("Co wybierasz? : ")
 while operacja != "0":
     if operacja=="1": zamiana_duze()
     elif operacja=="2": zamiana_male()
-    elif operacja=="3": zamiana_inne()
+    elif operacja=="3": zamiana_znaku()
     else: print("Wpisałeś niewłaściwą liczbę")
+    wstep()
+    menu()
+    operacja = input("Co wybierzesz ? ")
