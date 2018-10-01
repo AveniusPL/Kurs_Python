a54e8eb818f9cd884d5fa7de4cffaceda139e95f
Patrik <patryk.janusz@interia.pl>
2018-10-01 19:04:39 +0200

    Aktualizacja instrukcji,

--- Zadania_domowe/Rok_przestepny.py
+++ Zadania_domowe/Rok_przestepny.py
@@ -3,7 +3,7 @@ rok = int( input("Podaj aktualny rok"))
 def rok_przestepny():
 
     global rok
-    if rok % 4 == 0 and rok % 100 !=0:
+    if (rok % 4 == 0 and rok % 100 !=0) or rok % 400 == 0:
         print("Rok jest przestępny")
     else:
         print("Rok nie jest przetepny")
